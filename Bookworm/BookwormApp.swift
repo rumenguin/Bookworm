9cc106f80d6b2dd830541134e33f875cc8f98e2d
Rumen Guin <rumen2k@gmail.com>
2023-02-05 18:42:34 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by RUMEN GUIN on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
